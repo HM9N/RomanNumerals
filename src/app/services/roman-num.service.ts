32c279d46bf892211e7bb7faaafcbd252b851faf
HM9 <harry.e30s@gmail.com>
2020-12-17 02:04:23 -0500

    finalizado

--- src/app/services/roman-num.service.ts
+++ src/app/services/roman-num.service.ts
@@ -11,8 +11,8 @@ export class RomanNumService {
 
   covertToRoman(num: any) {
     this.result= '';
-    var arabicNumbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-    var romanSymbols = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'LX', 'XL', 'X','IX', 'V', 'IV', 'I'];
+    let arabicNumbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    let romanSymbols = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'LX', 'XL', 'X','IX', 'V', 'IV', 'I'];
     arabicNumbers.forEach((arabic, pos) => {
        while (num >= arabic) {
          num = num - arabic;
